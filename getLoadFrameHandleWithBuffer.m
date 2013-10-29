a39c6182dad1d2ef17703812abd76dc29a0d5a7a
Andrew Leifer <leifer@princeton.edu>
2013-10-29 12:58:27 -0400

    added some documentation

--- getLoadFrameHandleWithBuffer.m
+++ getLoadFrameHandleWithBuffer.m
@@ -1,7 +1,18 @@
 function h=getLoadFrameHandleWithBuffer(basefilename,extension,channelPrefix,filenameDigits,findNeuronsInRed,greenChIs1,FirstImNum,LastImNum)
+%This is a little crazy here. So basically this function returns a handle
+%to another function that the tracker will call everytime it wants to load
+%a frame.
+
+
+%This is the handle to the function below, loadFrame
 h=@loadFrame;
 
-
+    %This is a function that generates a filename based on an ID number.
+    %This function is required by the buffer machinery. It's what allows
+    %the buffer to look a few frames forward and backward and then load
+    %them from the filesystem. This will get turned into a function handle
+    %when everything is initialized and will get passed into the buffer
+    %machinery.
     function filename=getFileNameFromNumber(num)
         filename=[basefilename sprintf(['%.' num2str(filenameDigits) 'd'],num)  extension];
 
@@ -9,6 +20,8 @@ h=@loadFrame;
 
 
 
+    %This is the function that the tracker calls whenever it wants a new
+    %frame.
     function [I, ret]=loadFrame(num)
         
                 
@@ -37,7 +50,7 @@ h=@loadFrame;
             
             
             
-            %Load in a frame
+            %Load in a frame by asking the buffer machinery to provide it
             I=circBuff.loadFile(num);
 
             ret=false; %We are in range
